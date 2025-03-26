1704f6c0963cc94a0370af083432b3e9b10e0179
Chirayu Sahu <chirayuajaysahu01@gmail.com>
2025-03-26 16:25:49 +0530

    added smooth scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
+'use client'
+
 import Hero from "@/components/Hero";
 import {Grid} from "@/components/Grid";
 import AboutMe from "@/components/about-me";
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 export default function Home() {
+
+  useEffect(()=>{
+    const lenis = new Lenis();
+    
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    
+    requestAnimationFrame(raf);
+  },[])
+
   return (
     <>
     <Hero />
